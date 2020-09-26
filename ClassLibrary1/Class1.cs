1efd575cfaa8b845474cb66b4dd4c67aa604d348
Volodymyr S <arto.programmer@gmail.com>
2020-09-26 16:02:30 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public static class Calc
+    {
+        public static int sum(int a, int b) => a + b;
+
+    }
+}
---
+++ leson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 10;
+            int c = 20;
+            Console.WriteLine("a + b = {0}", Calc.sum(d,c));
+            Console.WriteLine("hello friend");
+        }
+    }
+
+}
